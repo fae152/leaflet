1b0ae18f8d4a20f1010592a5ed51c7224cc50fe9
fae152 <sarahmichiru@gmail.com>
2025-07-18 19:09:46 -0700

    update

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1y0IWTukXNpBAJKe5gY0JUdFxYzxrVx4hA5F4KpoYu9k/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyDdo64yQk5zwwqwfcxd-aNSWZoI-RCfk-Y';
+var googleApiKey = '';
